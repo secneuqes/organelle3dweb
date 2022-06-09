5ff8668641a811d436e51953dead4bf5ef6b33fd
0917ba <0917ba@naver.com>
2022-06-09 20:51:59 +0900

    6/9
    fov issue solved

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const mv = document.querySelector("#model-viewer");
-const annotationClicked = (a) => {
-  let ds = a.parentNode.dataset;
-  mv.cameraTarget = ds.target;
-  mv.cameraOrbit = ds.orbit;
-  if (ds.fov !== undefined) mv.fieldOfView = ds.fov;
-};
