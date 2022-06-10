7731499ca6a6865c2579c1bea474dd3f3e14a44e
박준서 <77626115+kingsparkdev@users.noreply.github.com>
2022-06-10 18:01:18 +0900

    6/10
    거의 다 완성함

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+// let btns = document.getElementsByClassName("HotspotAnnotation");
+// for(let i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener('click', function() {
+//         annotationClicked()
+//         modal('my_modal');
+        
+//     });
+// }
